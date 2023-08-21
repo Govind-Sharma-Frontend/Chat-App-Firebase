623d254cef56c19dca1ca9c957b468024c00a69c
Govind Sharma <govind.sharma@squarebits.com>
2023-08-21 19:01:00 +0530

    route fix

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LoginPage from "./Component/loginPage"
 // import Dashboard from "./Component/dashboard"
 import { auth } from "./firebase";
